e7a4b754ea5200c045cd418b4e24e13cd9792d71
ace <andrew.emil@gmail.com>
2017-07-21 20:47:52 -0400

    Added controllers for handling different deformations

--- Assets/Scripts/DeformationController.cs
+++ Assets/Scripts/DeformationController.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DeformMode
+{
+	Off,
+	Rumble,
+	Wave
+}
+
+
 public class DeformationController : MonoBehaviour {
 
+	private DeformMode deformMode;
+	private bool isOff;
 	// Use this for initialization
 	void Start () {
-		
+		deformMode = DeformMode.Rumble;
+		isOff = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		switch (deformMode) {
+		case DeformMode.Off:
+			DoOff ();
+			break; //Noop
+		case DeformMode.Rumble:
+			BasicRumble (false);
+			break;
+		}
+
+		if (deformMode != DeformMode.Off)
+			isOff = false;
+	}
+	public void UpdateDeformMode(DeformMode newDeformMode) {
+		deformMode = newDeformMode;
+	}
+
+	void DoOff() {
+		if (isOff) {
+			return;
+		}
+		BasicRumble (true);
+		isOff = true;
+	}
+
+	void BasicRumble(bool zero) {
 		MeshFilter meshFilter = gameObject.GetComponent<MeshFilter> ();
 		Mesh mesh = meshFilter.mesh;
 		Vector3[] baseVerticies = mesh.vertices;
@@ -20,6 +56,8 @@ public class DeformationController : MonoBehaviour {
 		for (var i=0; i < baseVerticies.Length; i++) {
 			var vertex = baseVerticies[i];
 			float noise = Mathf.PerlinNoise(timez + vertex.x, timez + vertex.y);
+			if (zero)
+				noise = 0f;
 			vertex.z = noise;
 			vertices[i] = vertex;
 		}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MoveMode
+{
+	Stop,
+	Move
+}
+
+
 public class PlayerController : MonoBehaviour {
 	private GameObject currentWaypoint;
 	private float speed;
 	private IList<string> path;
+	private MoveMode moveMode;
+	DeformationController deformationController;
 	void Start () {
 		Physics.gravity = new Vector3(0, -0.2F, 0);
 		Physics.bounceThreshold = 0;
+		speed = 0.029f;
+		InitMovement ();
+		deformationController = GameObject.Find ("MovingGrid").GetComponent<DeformationController> ();
+	}
+
+	private void InitMovement() {
 		GetPath ();
 		currentWaypoint = GameObject.Find ("W0");
-		speed = 0.029f;
+		moveMode = MoveMode.Move;
 	}
 
 	private void GetPath() {
@@ -26,29 +41,32 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	private void HandleMovement() {
-		if (AtTarget ()) {
-			MaybeUpdateTarget ();
-		} else {
-			DoMovement ();
+		if (moveMode == MoveMode.Move) {
+			if (AtTarget ()) {
+				if (InTerminalPosition ()) {
+					moveMode = MoveMode.Stop;
+					StopTrigger ();
+				} else {
+					UpdateTarget ();
+				}
+			} else {
+				DoMovement ();
+			}
 		}
+	}
+
+	private void StopTrigger() {
+		deformationController.UpdateDeformMode (DeformMode.Off);
+		//Start Timer player
 	}
 
 	private bool AtTarget() {
 		return Vector3.Distance (transform.position, currentWaypoint.transform.position) < 1;
 	}
 
-	private void MaybeUpdateTarget() {
+	private void UpdateTarget() {
 		//TODO delay on moving to next target
-		if(ShouldUpdateTarget()) {
-			currentWaypoint = GameObject.Find (path [path.IndexOf (currentWaypoint.name) + 1]);
-		}
-	}
-
-	private bool ShouldUpdateTarget() {
-		if (InTerminalPosition ()) {
-			return false;
-		}
-		return true;
+		currentWaypoint = GameObject.Find (path [path.IndexOf (currentWaypoint.name) + 1]);
 	}
 
 	private bool InTerminalPosition() {
